5b665d93d05bef4ddf52f9176b05e54df2e4831b
yattdev <yattdeveloper@gmail.com>
2021-12-01 23:02:55 +0100

    Create a component for template footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="footer">
+        <div className="container text-center">
+          <small className="copyright">
+            Designed with <i className="fas fa-heart"></i> by{" "}
+            <a href="https://themes.3rdwavemedia.com" target="blank">
+              Xiaoying Riley
+            </a>
+            for developers
+          </small>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
