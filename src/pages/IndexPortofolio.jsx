6ceec88984ae48f24b89642b3e0bcd9bdd41e036
yattdev <yattdeveloper@gmail.com>
2021-12-01 23:13:48 +0100

    Create IndexPortofolio a component that's wrapper for Portofolio template'

---
+++ src/pages/IndexPortofolio.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import PortofolioHeader from "../components/PortofolioHeader";
+import PortofolioFooter from "../components/PortofolioFooter";
+import Portofolio from "../components/Portofolio";
+
+const Index = () => {
+  return (
+    <>
+      <PortofolioHeader />
+      <Portofolio />
+      <PortofolioFooter />
+    </>
+  );
+};
+
+export default Index;
