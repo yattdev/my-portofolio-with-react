d72e3c6bb036d096a4a2c62be4651f50d0ca9773
yattdev <yattdeveloper@gmail.com>
2021-12-03 00:46:18 +0100

    Update InfoContactSection components with my data

--- src/components/secondary-sections/InfoContactSection.jsx
+++ src/components/secondary-sections/InfoContactSection.jsx
@@ -5,22 +5,22 @@ const InfoContactSection = () => {
     <main>
       <aside className="info aside section">
         <div className="section-inner shadow-sm rounded">
-          <h2 className="heading sr-only">Basic Information</h2>
+          <h2 className="heading sr-only">Information:</h2>
           <div className="content">
             <ul className="list-unstyled">
               <li>
                 <i className="fas fa-map-marker-alt"></i>
-                <span className="sr-only">Location:</span>San Francisco, US
+                <span className="sr-only">Location:</span>Casablanca, Morroco
               </li>
               <li>
                 <i className="fas fa-envelope"></i>
                 <span className="sr-only">Email:</span>
-                <a href="https://github.com/yattdev">jameslee@website.com</a>
+                <a href="https://github.com/yattdev">alassane@yatt.tech</a>
               </li>
               <li>
                 <i className="fas fa-link"></i>
                 <span className="sr-only">Website:</span>
-                <a href="https://github.com/yattdev">https://www.website.com</a>
+                <a href="https://github.com/yattdev">https://www.yatt.tech</a>
               </li>
             </ul>
           </div>
