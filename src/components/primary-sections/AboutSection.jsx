4f056f70e42d4d6b183da610137f57831dadbaa8
yattdev <yattdeveloper@gmail.com>
2021-12-03 12:49:56 +0100

    Update AboutSection with my data

--- src/components/primary-sections/AboutSection.jsx
+++ src/components/primary-sections/AboutSection.jsx
@@ -7,13 +7,34 @@ const AboutSection = () => {
         <div className="section-inner shadow-sm rounded">
           <h2 className="heading">About Me</h2>
           <div className="content">
-            <p>
-              Write a brief intro about yourself. It's a good idea to include
-              your personal interests and hobbies as well. Lorem ipsum dolor sit
-              amet, consectetuer adipiscing elit. Aenean commodo ligula eget
-              dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis
-              parturient montes, nascetur ridiculus mus. Donec quam felis,
-              ultricies nec. Commodo ligula eget dolor. Aenean massa.
+            <p className="text-start">
+              Alassane Yattara is an experienced software engineer and
+              passionate Starter (Creator). Alassane currently holds a master's
+              degree in computer science from the Institut Polytechnique Privé
+              in Casablanca, Morocco, after obtaining his bachelor's degree in
+              Mathematics, Physics and Chemistry in
+              <a href="https://fr.wikipedia.org/wiki/Bamako">Bamako, Mali</a>.
+              <br />
+              <br />
+              Committed starter, Detail-oriented engineer, Manager, Alassane has
+              designed, implemented and tested complex web applications,
+              <a href="https://fr.wikipedia.org/wiki/Representational_state_transfer">
+                RESTFULL API
+              </a>
+              , Intelligent Personal Assistant (
+              <a href="https://towardsdatascience.com/natural-language-processing-nlp-for-machine-learning-d44498845d5b">
+                NPL
+              </a>
+              ) and experienced Machine Learning (ML) in the field of artificial
+              intelligence (AI), using a variety of programming languages and
+              technologies. Alassane also learns quickly with strong
+              problem-solving, leadership, management and communication skills,
+              comfortable working in a team or individually.
+              <br />
+              <br />
+              In his spare time, Alassane absorbs books, shares this knowledge,
+              contributes to the community in daily actions and also develops
+              <a href="https://github.com/yattdev/">Open source</a> software.
             </p>
           </div>
         </div>
