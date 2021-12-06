c54ed71282d9e996c071a1deb7d68cde67a51a4b
yattdev <yattdeveloper@gmail.com>
2021-12-06 14:39:26 +0100

    Update LastPostSection to add my personal data

--- src/components/secondary-sections/LastPostSection.jsx
+++ src/components/secondary-sections/LastPostSection.jsx
@@ -7,11 +7,14 @@ const LastPostSection = () => {
         <div className="section-inner shadow-sm rounded">
           <h2 className="heading">Latest Blog Posts</h2>
           <p>
-            You can use Sascha Depold's
-            <a href="https://github.com/sdepold/jquery-rss" target="blank">
-              Vanilla RSS plugin
+            How to install &nbsp;
+            <a
+              href="https://yattdeveloper.medium.com/how-to-install-vim-with-clipboard-on-ubuntu18-to-20-x-5cc4970eb3dc"
+              target="blank"
+            >
+              Vim with +clipboard &nbsp;
             </a>
-            to pull in your blog post feeds.
+            on Ubuntu 20.x
           </p>
           <div id="rss-feeds" className="content"></div>
         </div>
