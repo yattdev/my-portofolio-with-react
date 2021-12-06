35789757ed44d7ed8657b7361a7b85fdf5484582
yattdev <yattdeveloper@gmail.com>
2021-12-06 21:08:29 +0100

    Update primary-sections/AboutSection correct of mistake on text

--- src/components/primary-sections/AboutSection.jsx
+++ src/components/primary-sections/AboutSection.jsx
@@ -10,26 +10,26 @@ const AboutSection = () => {
             <p className="text-start">
               Alassane Yattara is an experienced software engineer and
               passionate Starter (Creator). Alassane currently holds a master's
-              degree in computer science from the Institut Polytechnique Privé
-              in Casablanca, Morocco, after obtaining his bachelor's degree in
-              Mathematics, Physics and Chemistry in &nbsp;
+              degree in computer science from Institute Polytechnic Private of
+              Casablanca, Morocco. After his bachelor's degree in Mathematics,
+              Physics and Chemistry in &nbsp;
               <a href="https://fr.wikipedia.org/wiki/Bamako">Bamako, Mali</a>.
               <br />
               <br />
               Committed starter, Detail-oriented engineer, Manager, Alassane has
               designed, implemented and tested complex web applications, &nbsp;
               <a href="https://fr.wikipedia.org/wiki/Representational_state_transfer">
-                RESTFULL API
+                Restfull API
               </a>
-              , Intelligent Personal Assistant (
+              , Assistant personnel intelligent (Virtual assistant) in &nbsp;
               <a href="https://towardsdatascience.com/natural-language-processing-nlp-for-machine-learning-d44498845d5b">
                 NPL
               </a>
-              ) and experienced Machine Learning (ML) in the field of artificial
-              intelligence (AI), using a variety of programming languages and
-              technologies. Alassane also learns quickly with strong
-              problem-solving, leadership, management and communication skills,
-              comfortable working in a team or individually.
+              &nbsp; and experienced Machine Learning (ML), using a variety of
+              programming languages and technologies. Alassane also learns
+              quickly with strong problem-solving, leadership, management and
+              communication skills, comfortable working in a team or
+              individually.
               <br />
               <br />
               In his spare time, Alassane absorbs books, shares this knowledge,
