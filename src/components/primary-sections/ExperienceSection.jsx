5990eaa344facfe7e550bbc68420e127f6e1c1e8
yattdev <yattdeveloper@gmail.com>
2021-12-06 17:48:44 +0100

    Update ExperienceSection with my own data

--- src/components/primary-sections/ExperienceSection.jsx
+++ src/components/primary-sections/ExperienceSection.jsx
@@ -9,47 +9,90 @@ const Experience = () => {
           <div className="content">
             <div className="item">
               <h3 className="title">
-                Co-Founder & Lead Developer -
+                Machine Learning Developer &amp; AI Developer - &nbsp;
                 <span className="place">
-                  <a href="https://github.com/yattdev">Startup Hub</a>
+                  <a href="https://github.com/yattdev">PFE Project</a> &nbsp;
                 </span>
-                <span className="year">(2020 - Present)</span>
+                <span className="year">(Casablanca, Maroc)(2020 - 2021)</span>
               </h3>
               <p>
-                Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac,
-                enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a,
-                tellus. Phasellus viverra nulla ut metus varius laoreet. Donec
-                vitae sapien ut libero venenatis faucibus. Nullam quis ante.
-                Etiam sit amet orci eget eros faucibus tincidunt.
+                As an Senior project, I decided to forge my own path, to the
+                design and build a smart virtual personal assistant named Jady,
+                for entrepreneurs. The motivation for this personal initiative
+                was to design something that would make a real difference in
+                people's lives. something more than the design of the hundredth
+                websites or applications.
+                <br />
+                <strong>Technologies Used: &nbsp;</strong>
+                <em>
+                  Python, Django, DRF, Rasa Open Source, Json, Yaml, Spacy,
+                  Tensorflow, Rasa WebChat, Search Engine Librairy Whoosh, NLU
+                  data generator, HTML, CSS, Javascript, Typescript, Angular,
+                  Git, GitHub
+                </em>
               </p>
             </div>
             <div className="item">
               <h3 className="title">
-                Software Engineer -
+                Software Developer - &nbsp;
                 <span className="place">
-                  <a href="https://github.com/yattdev">Google</a>
+                  <a href="https://github.com/yattdev">Smartech-Expert</a>{" "}
+                  &nbsp;
                 </span>
-                <span className="year">(2019 - 2020)</span>
+                <span className="year">(Casablanca, Maroc)(2019 - 2020)</span>
               </h3>
               <p>
-                Vivamus a tortor eu turpis pharetra consequat quis non metus.
-                Aliquam aliquam, orci eu suscipit pellentesque, mauris dui
-                tincidunt enim. Sed fringilla mauris sit amet nibh. Donec
-                sodales sagittis magna.
+                Consolidate its position in the sector and respond to market
+                demand. My mission within Smartech was to develop version 2.0 of
+                a scoring and reporting application for companies in
+                collaboration with the co-founder of the company.
+                <br />
+                <strong>Technologies Being Used: &nbsp;</strong>
+                <em>Php, Laravel, HTM, CSS, JAVASCRIPT, Git, Bitbucket</em>
               </p>
             </div>
-
             <div className="item">
               <h3 className="title">
-                Software Engineer -
+                Software Developer and Fonctional Tester - &nbsp;
                 <span className="place">
-                  <a href="https://github.com/yattdev">eBay</a>
+                  <a href="https://github.com/yattdev">Accurate</a> &nbsp;
                 </span>
-                <span className="year">(2018 - 2019)</span>
+                <span className="year">(Casablanca, Maroc)(2018 - 2019)</span>
               </h3>
               <p>
-                Maecenas tempus, tellus eget condimentum rhoncus, sem quam
-                semper libero, sit amet adipiscing sem neque sed ipsum.
+                The testing purpose was to increase backend server stability and
+                test whole application for real use case. Detect anomalies,
+                establish a functional diagnostic schedule, reporting a shortand
+                long-term integration test plan that will allow them to achieve
+                the established objective.
+                <br />
+                <strong>Technologies Being Used: &nbsp;</strong>
+                <em>
+                  Java, Groovy, Selenium, JMeter, Katalon Stadio, HTM, CSS,
+                  JAVASCRIPT
+                </em>
+              </p>
+            </div>
+            <div className="item">
+              <h3 className="title">
+                Remote Web Developer - &nbsp;
+                <span className="place">
+                  <a href="https://github.com/yattdev">Online</a> &nbsp;
+                </span>
+                <span className="year">
+                  (Casablanca, Maroc)(2018 - Present)
+                </span>
+              </h3>
+              <p>
+                As a freelance developer i'm enjoy on all projects i choose to
+                build with a strong desire to put my skills at the service of
+                various projects requiring a rapid but rigorous development.
+                <br />
+                <strong>Technologies Used: &nbsp;</strong>
+                <em>
+                  Python, Django, DRF, Reactjs, Vuejs, HTML, CSS, JAVASCRIPT
+                  TYPESCRIPT, AWS, Heroku, Celery.
+                </em>
               </p>
             </div>
           </div>
