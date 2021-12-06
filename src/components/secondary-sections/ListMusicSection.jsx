0eeb637a81ec58d1b984378cbb3d4b11ca1db7ad
yattdev <yattdeveloper@gmail.com>
2021-12-06 14:57:24 +0100

    Update ListMusicSection to add my favorite music

--- src/components/secondary-sections/ListMusicSection.jsx
+++ src/components/secondary-sections/ListMusicSection.jsx
@@ -10,25 +10,33 @@ const ListMusicSection = () => {
             <ul className="list-unstyled">
               <li>
                 <i className="fas fa-headphones"></i>
-                <a href="https://backenk.yatt.tech">
-                  Etiam hendrerit urna nunc
+                <a href="https://www.youtube.com/watch?v=Ny8YAnmpW7U">
+                  Travis x Drake - Riot Ft ASAP
                 </a>
               </li>
               <li>
                 <i className="fas fa-headphones"></i>
-                <a href="https://backenk.yatt.tech">
-                  Ut sollicitudin in mauris non auctor
+                <a href="https://www.youtube.com/watch?v=ss7EJ-PW2Uk">
+                  Instrumental Music
                 </a>
               </li>
               <li>
                 <i className="fas fa-headphones"></i>
-                <a href="https://backenk.yatt.tech">
-                  Etiam hendrerit urna nunc
+                <a href="https://www.youtube.com/watch?v=rjctVZVM14s">
+                  Travis x Drake - Goku Ft 21 Savage
                 </a>
               </li>
               <li>
                 <i className="fas fa-headphones"></i>
-                <a href="https://backenk.yatt.tech">Duis et felis bibendum</a>
+                <a href="https://www.youtube.com/watch?v=eCs8LT290a4">
+                  Deep Programming - Modern VIBE
+                </a>
+              </li>
+              <li>
+                <i className="fas fa-headphones"></i>
+                <a href="https://www.youtube.com/watch?v=M5QY2_8704o">
+                  Chillstep Music pour la programmation
+                </a>
               </li>
             </ul>
           </div>
