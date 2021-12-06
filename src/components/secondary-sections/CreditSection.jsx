3ca512f6458f41c95895647c7a716ca7d25a9798
yattdev <yattdeveloper@gmail.com>
2021-12-06 14:35:39 +0100

    Update credit section to clean uneccessairy data

--- src/components/secondary-sections/CreditSection.jsx
+++ src/components/secondary-sections/CreditSection.jsx
@@ -52,59 +52,7 @@ const CreditSection = () => {
                 </a>
               </li>
             </ul>
-
             <hr />
-
-            <p className="pt-2">
-              This Boootstrap portfolio template is handcrafted by UX designer
-              <a href="https://twitter.com/3rdwave_themes" target="blank">
-                Xiaoying Riley
-              </a>
-              at
-              <a href="https://themes.3rdwavemedia.com/" target="blank">
-                3rd Wave Media
-              </a>
-              for developers and is <strong>FREE</strong> as long as you keep
-              the footer attribution link.
-            </p>
-            <p>
-              If you’d like to
-              <strong>use this template without the attribution link</strong>,
-              you can
-              <a
-                href="https://themes.3rdwavemedia.com/bootstrap-templates/resume/free-bootstrap-portfolio-theme-for-web-developers/"
-                target="blank"
-              >
-                buy the commercial license
-              </a>
-              .
-            </p>
-
-            <a
-              className="btn btn-cta-primary btn-download"
-              href="https://themes.3rdwavemedia.com/bootstrap-templates/resume/free-bootstrap-portfolio-theme-for-web-developers/"
-              target="blank"
-            >
-              <i className="fas fa-download"></i> I want to download
-            </a>
-            <hr />
-            <h5 className="subheading pt-3">
-              Want more free Bootstrap themes and templates?
-            </h5>
-            <p>
-              Check out our
-              <a href="https://themes.3rdwavemedia.com/bootstrap-templates/free/">
-                collection of free themes and templates.
-              </a>
-            </p>
-            <p>
-              Follow
-              <a href="https://twitter.com/3rdwave_themes" target="blank">
-                Xiaoying
-              </a>
-              on twitter so you don't miss any future freebies!
-            </p>
-
             <a
               className="btn btn-cta-secondary btn-follow"
               href="https://twitter.com/3rdwave_themes"
