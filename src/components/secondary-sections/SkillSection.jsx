b3a8841754881dc614f6cc188502198485ac957c
yattdev <yattdeveloper@gmail.com>
2021-12-08 19:07:31 +0100

    Update SkillSection to complete my skills and toools stack

--- src/components/secondary-sections/SkillSection.jsx
+++ src/components/secondary-sections/SkillSection.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { MDBIcon } from "mdb-react-ui-kit";
 
 const SkillSection = () => {
   return (
@@ -110,17 +111,220 @@ const SkillSection = () => {
                   ></div>
                 </div>
               </div>
-
-              <p>
-                <a
-                  className="more-link"
-                  href="https://www.linkedin.com/in/alassane-yattara-587477166/"
-                >
-                  <i className="fas fa-external-link-alt"></i>More on Linkedin
-                </a>
+            </div>
+          </div>
+          <hr />
+          <blockquote>
+            <q>
+              My super power is my ability to learn and experience new things
+            </q>
+          </blockquote>
+          <div className="content">
+            <div className="item">
+              {/* Backend web tools */}
+              <p style={{ textIndent: "-0.6em" }}>
+                <strong style={{ fontSize: "1.1em" }}>
+                  Backend Web: &nbsp;&nbsp;
+                  <MDBIcon fab icon="python" />
+                  <MDBIcon fab icon="js" />
+                  <MDBIcon fas icon="bug" />
+                  <MDBIcon fab icon="node-js" />
+                </strong>
+                <br />
+                <strong>Python: </strong>
+                <span>
+                  Django, Django Rest Framework, Flask, FastAPI, Sanic
+                  Framework.
+                </span>
+                <br />
+                <strong>Javascript/Typescript: </strong>
+                <span>NodeJs</span>
+                <br />
+                <strong>Testing:</strong> Unit-tests, JMeter
+              </p>
+              {/* Frontend web tools */}
+              <p style={{ textIndent: "-0.6em" }}>
+                <strong style={{ fontSize: "1.1em" }}>
+                  Frontend Web &amp; CMS: &nbsp;&nbsp;
+                  <MDBIcon fab icon="react" />
+                  <MDBIcon fab icon="vuejs" />
+                  <MDBIcon fab icon="mdb" />
+                  <MDBIcon fab icon="angular" />
+                  <MDBIcon fab icon="wordpress" />
+                </strong>
+                <br />
+                <strong>Javascript/Typescript: </strong>
+                <span>ReactJS, VueJS, AngularJS</span>
+                <br />
+                <strong>CMS: </strong>
+                <span>Django CMS, WordPress, Prestashop</span>
+                <br />
+                <strong>Testing:</strong> Selenium, Katalon
+              </p>
+              {/* UI Design web tools */}
+              <p style={{ textIndent: "-0.6em" }}>
+                <strong style={{ fontSize: "1.1em" }}>
+                  UI Design: &nbsp;&nbsp;
+                  <MDBIcon fab icon="figma" />
+                  <MDBIcon fab icon="sketch" />
+                  <MDBIcon fab icon="microsoft" />
+                </strong>
+                <br />
+                <strong>UI/Prototyping: </strong>
+                <span>Photoshop, Figma</span>
+                <br />
+                <strong>Presentation: </strong>
+                <span>Canva, Microsoft PowerPoint</span>
+                <br />
+                <strong>Testing:</strong> Unit tests, JMeter
+              </p>
+              {/* Databases tools */}
+              <p style={{ textIndent: "-0.6em" }}>
+                <strong style={{ fontSize: "1.1em" }}> Databases:</strong>
+                <br />
+                <MDBIcon fab icon="python" />
+                <strong>Python: </strong>
+                <span>
+                  Django, Django Rest Framework, Machine Learning, NLP, Rasa
+                  Open Source, FastAPI, Flask, Sanic Framework, Search Engine
+                  Whoosh.
+                </span>
+                <br />
+                <MDBIcon fab icon="python" />
+                <strong>Javascript/Typescript: </strong>
+                <span>NodeJs</span>
+                <br />
+                <MDBIcon fab icon="python" />
+                <strong>Testing:</strong> Unit tests, JMeter
+              </p>
+              {/* Artificial Intelligence tools */}
+              <p style={{ textIndent: "-0.6em" }}>
+                <strong style={{ fontSize: "1.1em" }}>
+                  Artificial Intelligence:
+                </strong>
+                <br />
+                <MDBIcon fab icon="python" />
+                <strong>Python: </strong>
+                <span>
+                  Django, Django Rest Framework, Machine Learning, NLP, Rasa
+                  Open Source, FastAPI, Flask, Sanic Framework, Search Engine
+                  Whoosh.
+                </span>
+                <br />
+                <MDBIcon fab icon="python" />
+                <strong>Javascript/Typescript: </strong>
+                <span>NodeJs</span>
+                <br />
+                <MDBIcon fab icon="python" />
+                <strong>Testing:</strong> Unit tests, JMeter
+              </p>
+              {/* DevOps tools */}
+              <p style={{ textIndent: "-0.6em" }}>
+                <strong style={{ fontSize: "1.1em" }}> DevOps:</strong>
+                <br />
+                <MDBIcon fab icon="python" />
+                <strong>Python: </strong>
+                <span>
+                  Django, Django Rest Framework, Machine Learning, NLP, Rasa
+                  Open Source, FastAPI, Flask, Sanic Framework, Search Engine
+                  Whoosh.
+                </span>
+                <br />
+                <MDBIcon fab icon="python" />
+                <strong>Javascript/Typescript: </strong>
+                <span>NodeJs</span>
+                <br />
+                <MDBIcon fab icon="python" />
+                <strong>Testing:</strong> Unit tests, JMeter
+              </p>
+              {/* IDE tools */}
+              <p style={{ textIndent: "-0.6em" }}>
+                <strong style={{ fontSize: "1.1em" }}> IDE:</strong>
+                <br />
+                <MDBIcon fab icon="python" />
+                <strong>Python: </strong>
+                <span>
+                  Django, Django Rest Framework, Machine Learning, NLP, Rasa
+                  Open Source, FastAPI, Flask, Sanic Framework, Search Engine
+                  Whoosh.
+                </span>
+                <br />
+                <MDBIcon fab icon="python" />
+                <strong>Javascript/Typescript: </strong>
+                <span>NodeJs</span>
+                <br />
+                <MDBIcon fab icon="python" />
+                <strong>Testing:</strong> Unit tests, JMeter
+              </p>
+              {/* Operating Systems tools */}
+              <p style={{ textIndent: "-0.6em" }}>
+                <strong style={{ fontSize: "1.1em" }}>
+                  Operating Systems:
+                </strong>
+                <br />
+                <MDBIcon fab icon="python" />
+                <strong>Python: </strong>
+                <span>
+                  Django, Django Rest Framework, Machine Learning, NLP, Rasa
+                  Open Source, FastAPI, Flask, Sanic Framework, Search Engine
+                  Whoosh.
+                </span>
+                <br />
+                <MDBIcon fab icon="python" />
+                <strong>Javascript/Typescript: </strong>
+                <span>NodeJs</span>
+                <br />
+                <MDBIcon fab icon="python" />
+                <strong>Testing:</strong> Unit tests, JMeter
+              </p>
+              {/* Other Skills tools */}
+              <p style={{ textIndent: "-0.6em" }}>
+                <strong style={{ fontSize: "1.1em" }}> Other Skills:</strong>
+                <br />
+                <MDBIcon fab icon="python" />
+                <strong>Python: </strong>
+                <span>
+                  Django, Django Rest Framework, Machine Learning, NLP, Rasa
+                  Open Source, FastAPI, Flask, Sanic Framework, Search Engine
+                  Whoosh.
+                </span>
+                <br />
+                <MDBIcon fab icon="python" />
+                <strong>Javascript/Typescript: </strong>
+                <span>NodeJs</span>
+                <br />
+                <MDBIcon fab icon="python" />
+                <strong>Testing:</strong> Unit tests, JMeter
+              </p>
+              {/* Methodologies tools */}
+              <p style={{ textIndent: "-0.6em" }}>
+                <strong style={{ fontSize: "1.1em" }}> Methodologies:</strong>
+                <br />
+                <MDBIcon fab icon="python" />
+                <strong>Python: </strong>
+                <span>
+                  Django, Django Rest Framework, Machine Learning, NLP, Rasa
+                  Open Source, FastAPI, Flask, Sanic Framework, Search Engine
+                  Whoosh.
+                </span>
+                <br />
+                <MDBIcon fab icon="python" />
+                <strong>Javascript/Typescript: </strong>
+                <span>NodeJs</span>
+                <br />
+                <MDBIcon fab icon="python" />
+                <strong>Testing:</strong> Unit tests, JMeter
               </p>
             </div>
           </div>
+          <p>
+            <a
+              className="more-link"
+              href="https://www.linkedin.com/in/alassane-yattara-587477166/"
+            >
+              <i className="fas fa-external-link-alt"></i>More on Linkedin
+            </a>
+          </p>
         </div>
       </aside>
     </>
